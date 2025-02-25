96791100db0b64a5392b3279d404cd2b08d0f96e
spyrapiotr <spyrapiotr93@gmail.com>
2025-02-25 20:57:33 +0100

    Switch update

--- _layouts/theme-switcher.js
+++ _layouts/theme-switcher.js
@@ -1,7 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
   const themeSwitcher = document.getElementById('theme-switcher');
-  themeSwitcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme');
-    document.body.classList.toggle('light-theme');
+  const currentTheme = localStorage.getItem('theme') || 'light';
+
+  if (currentTheme === 'dark') {
+    document.body.classList.add('dark-theme');
+    document.body.classList.remove('light-theme');
+    themeSwitcher.checked = true;
+  } else {
+    document.body.classList.add('light-theme');
+    document.body.classList.remove('dark-theme');
+  }
+
+  themeSwitcher.addEventListener('change', function() {
+    if (themeSwitcher.checked) {
+      document.body.classList.add('dark-theme');
+      document.body.classList.remove('light-theme');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.body.classList.add('light-theme');
+      document.body.classList.remove('dark-theme');
+      localStorage.setItem('theme', 'light');
+    }
   });
 });
