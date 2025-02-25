c2b1079ae0e9bde51be525291dbba5c65f86695d
spyrapiotr <spyrapiotr93@gmail.com>
2025-02-25 20:38:49 +0100

    themes

--- _layouts/theme-switcher.js
+++ _layouts/theme-switcher.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const themeSwitcher = document.getElementById('theme-switcher');
-    themeSwitcher.addEventListener('click', function() {
-      document.body.classList.toggle('dark-theme');
-      document.body.classList.toggle('light-theme');
-    });
+  const themeSwitcher = document.getElementById('theme-switcher');
+  themeSwitcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
   });
+});
