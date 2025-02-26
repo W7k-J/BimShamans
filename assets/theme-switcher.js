42815f487ff2c645b38053030e2094ca777bce24
spyrapiotr <spyrapiotr93@gmail.com>
2025-02-26 18:33:50 +0100

    Grube zmieny xD

--- assets/theme-switcher.js
+++ assets/theme-switcher.js
@@ -1,33 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const themeSwitcher = document.getElementById('theme-switcher');
-  const currentTheme = localStorage.getItem('theme') || 'light';
-  const switchState = localStorage.getItem('switchState') || 'false';
+    const themeSwitcher = document.getElementById('theme-switcher');
+    const currentTheme = localStorage.getItem('theme') || 'light';
 
-  // Set initial theme and switch state
-  if (currentTheme === 'dark') {
-      document.body.classList.add('dark-theme');
-      document.body.classList.remove('light-theme');
-      themeSwitcher.checked = true;
-  } else {
-      document.body.classList.add('light-theme');
-      document.body.classList.remove('dark-theme');
-      themeSwitcher.checked = false;
-  }
+    if (currentTheme === 'dark') {
+        document.body.classList.add('dark-theme');
+        document.body.classList.remove('light-theme');
+        themeSwitcher.checked = true;
+    } else {
+        document.body.classList.add('light-theme');
+        document.body.classList.remove('dark-theme');
+    }
 
-  // Set switch position from localStorage
-  themeSwitcher.checked = switchState === 'true';
-
-  themeSwitcher.addEventListener('change', function() {
-      if (themeSwitcher.checked) {
-          document.body.classList.add('dark-theme');
-          document.body.classList.remove('light-theme');
-          localStorage.setItem('theme', 'dark');
-          localStorage.setItem('switchState', 'true');
-      } else {
-          document.body.classList.add('light-theme');
-          document.body.classList.remove('dark-theme');
-          localStorage.setItem('theme', 'light');
-          localStorage.setItem('switchState', 'false');
-      }
-  });
+    themeSwitcher.addEventListener('change', function() {
+        if (themeSwitcher.checked) {
+            document.body.classList.add('dark-theme');
+            document.body.classList.remove('light-theme');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.body.classList.add('light-theme');
+            document.body.classList.remove('dark-theme');
+            localStorage.setItem('theme', 'light');
+        }
+    });
 });
