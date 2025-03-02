ea949916c847218d8ec5df85e79121c775b8503b
spyrapiotr <spyrapiotr93@gmail.com>
2025-03-02 22:15:28 +0100

    55

--- assets/background-network.js
+++ assets/background-network.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("circuitCanvas");
+const canvas = document.getElementById("networkCanvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
