ef0e551f3e80d3792994333a76f23e852343a4df
spyrapiotr <spyrapiotr93@gmail.com>
2025-03-03 13:37:07 +0100

    65

--- assets/background-network.js
+++ assets/background-network.js
@@ -6,7 +6,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const paths = [];
-const numPaths = 50; // Ilość ścieżek
+const numPaths = 100; // Zwiększona ilość ścieżek
 const speed = 2; // Prędkość poruszania się impulsów
 let centerX = canvas.width * 0.5; // Środek x
 let centerY = canvas.height * 0.5; // Środek y
@@ -14,14 +14,14 @@ let centerY = canvas.height * 0.5; // Środek y
 // Tworzenie losowych ścieżek "procesora"
 for (let i = 0; i < numPaths; i++) {
   let angle = Math.random() * Math.PI * 2;
-  paths.push({ angle, progress: 0 });
+  let maxLength = Math.random() * Math.max(canvas.width, canvas.height);
+  paths.push({ angle, progress: 0, maxLength });
 }
 
 // Funkcja rysowania animacji
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeStyle = "rgba(255, 0, 179, 0.5)";
-  // ctx.strokeStyle = "rgba(0, 150, 255, 0.5)";
+  ctx.strokeStyle = "rgba(0, 150, 255, 0.5)";
   ctx.lineWidth = 2;
 
   paths.forEach((path) => {
@@ -41,7 +41,7 @@ function draw() {
     ctx.fill();
 
     path.progress += speed;
-    if (path.progress > Math.max(canvas.width, canvas.height)) {
+    if (path.progress > path.maxLength) {
       path.progress = 0;
     }
   });
